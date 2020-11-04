23ef8e00f5717def07190e4eebe124e77f9ffa9f
Gareth Redfern <gareth@garethredfern.com>
2020-11-04 22:20:37 +0000

    Update to use sanctum not airlock

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Laravel\Airlock\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
