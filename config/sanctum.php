549fa13b2202469a4a7737a5a68044abe38b77b4
Gareth Redfern <gareth@garethredfern.com>
2020-11-04 21:32:29 +0000

    Update sanctum config

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,9 +13,7 @@ return [
     |
     */
 
-    'stateful' => [
-        'localhost',
-    ],
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,127.0.0.1:8000,::1')),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,5 +27,21 @@ return [
     */
 
     'expiration' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Middleware
+    |--------------------------------------------------------------------------
+    |
+    | When authenticating your first-party SPA with Sanctum you may need to
+    | customize some of the middleware Sanctum uses while processing the
+    | request. You may change the middleware listed below as required.
+    |
+    */
+
+    'middleware' => [
+        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+    ],
 
 ];
